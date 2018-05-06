38c69c4f9ef055a4d1d868cb2b54d26118855f38
ShaulskayaV <36891261+ShaulskayaV@users.noreply.github.com>
2018-05-06 16:35:31 +0300

    Исправлены отступы.

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
 
 class Tetris(QMainWindow):
+
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -14,53 +15,61 @@ class Tetris(QMainWindow):
         self.tboard = Board(self)
         self.setCentralWidget(self.tboard)
 
-        self.statusbar =self.statusBar()
+        self.statusbar = self.statusBar()
         self.tboard.msg2Statusba[str].connect(self.statusbar.showMessage)
         self.tboard.start()
         self.resize(180,380)
         self.center()
         self.setWindowTitle('Tetris')
         self.show()
+
     def center(self):
-        screen= QDesktopWidget().screenGeometry()
-        size= self.geometry()
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
         self.move ((screen.width()-size.width())/2, (screen.height()-size.height())/2)
+
+
 class Board(QFrame):
-    msg2Statusbar= pyqtSignal(str)
+    msg2Statusbar = pyqtSignal(str)
     BoardWidth = 10
     BoardHeight = 22
     Speed = 300
+
     def __init__(self,parent):
         super().__init__(parent)
         self.initBoard()
 
     def initBoard(self):
         self.timer=QBasicTimer()
-        self.isWaitingAfterLime= False
-        self.curX=0
-        self.curY=0
-        self.numLinesRemoved=0
-        self.board=[]
+        self.isWaitingAfterLime = False
+        self.curX = 0
+        self.curY = 0
+        self.numLinesRemoved = 0
+        self.board = []
 
         self.setFocusPolicy(Qt.StrongFocus)
-        self.isStarted=False
-        self.isPaused=False
+        self.isStarted = False
+        self.isPaused = False
         self.clearBoard()
 
     def shapeAt(self,x,y):
         return self.board[(y*Board.BoardWidth)+x]
+
     def setShape(self,x,y,shape):
         self.board[(y*Board.BoardWidth)+x]= shape
+
     def squareWidth(self):
         return self.contentsRect().width()//Board.BoardWidth
+
     def squareHeight(self):
         return self.contentsRect().height()//Board.BoardHeight
+
     def start(self):
         if self.isPaused:
             return
         self.isStarted=True
-        self.isWaitingAfterLime=False
-        self.numLinesRemoved=0
+        self.isWaitingAfterLime = False
+        self.numLinesRemoved = 0
         self.clearBoard()
 
         self.msg2Statusbar.emit(str(self.numLinesRemoved))
